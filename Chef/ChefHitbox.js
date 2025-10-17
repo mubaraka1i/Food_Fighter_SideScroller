234843a93741e215b0498621457bf09e117acc3f
smit23sx <smit23sx@dukes.jmu.edu>
2025-10-17 11:02:34 -0400

    changed hitbox to a rectangle

--- Chef/ChefHitbox.js
+++ Chef/ChefHitbox.js
@@ -7,23 +7,23 @@ let visible;
 function setup() {
   createCanvas(400, 400);
   seaLevel = 0;
-  playerPosX = 20;
-  playerPosY = height - 20 - seaLevel;
-  hitboxDia = 25;
+  playerPosX = 25;
+  playerPosY = height - 50 - seaLevel;
+  hitboxLength = 25;
   visible = false;
 }
 
 function draw() {
   background(220);
-  playerHitbox(playerPosX, playerPosY, hitboxDia, visible);
+  playerHitbox(playerPosX, playerPosY, hitboxLength, visible);
 }
 
-function playerHitbox(x, y, diameter, visible) {
+function playerHitbox(x, y, length, visible) {
   noFill();
   if (visible == true) {
     stroke('black');
   } else {
     noStroke();
   }
-  circle(x, y, diameter);
+  rect(x, y, length, length);
 }
