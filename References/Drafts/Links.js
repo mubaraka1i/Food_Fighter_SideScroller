b998302e8288888e73947d13c9ce5846e2a0739e
mubaraka1i <ali25mx@dukes.jmu.edu>
2025-11-02 20:01:58 -0500

    Add Level 1 Kitchen Background reference

--- References/Drafts/Links.js
+++ References/Drafts/Links.js
@@ -1,2 +1,5 @@
 // Lucid Spark UML Diagram:
 // https://lucid.app/lucidspark/900b0d11-98e9-4e2c-9b6d-e5d991081e8b/edit?viewport_loc=-11%2C55%2C1707%2C777%2C0_0&invitationId=inv_42bb3cc9-b7a0-4ace-b966-fcadd7aca460
+
+// Level 1 Kitchen Background
+// https://dribbble.com/shots/19892926-Pixel-kitchen
