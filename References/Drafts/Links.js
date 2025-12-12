c05b294dbeb3c6bd1ae0b7dce13c3653aae0833a
mubaraka1i <ali25mx@dukes.jmu.edu>
2025-12-12 00:00:38 -0500

    Reference Add(Music)

--- References/Drafts/Links.js
+++ References/Drafts/Links.js
@@ -5,3 +5,7 @@
 // https://docs.google.com/drawings/d/1U1ZZkHWq8moSmn_6IgfsiQAr6cKkkcC1-AeOgecj0w4/edit?usp=sharing
 // https://docs.google.com/drawings/d/187vdyPuFq--q5hSdxfgNh3ZVxAFnKu2e8NFlls-5vCM/edit?usp=sharing
 // https://docs.google.com/drawings/d/1NeOU5yoTBRiNXx9I4Ff2lIFohKNMlhr3r9vuJQ077WU/edit?usp=sharing
+
+// Background Theme Music
+// https://stock.adobe.com/search/audio?album=&attribution=&current_page=1&duration_max=1200000&duration_min=0&electronic=Electronic&electronic=Industrial&electronic=Rave&electronic=Techno&electronic=Trance&genres=Electronic&keywords=&loop=false&order=relevance&page_limit=50&search_type=usertyped&tempo_max=300&tempo_min=1
+// https://pixabay.com/music/search/gaming/
